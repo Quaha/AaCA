6bf6e3a94de97e378dd4afa27ae95a95277562aa
Artem Romanov <artemromanov2017@yandex.ru>
2025-10-21 12:16:08 +0300

    Small fix

--- solution.cpp
+++ solution.cpp
@@ -262,6 +262,10 @@ std::pair<std::vector<Weight>, std::vector<idx_t>> fordBellman(const DirectedGra
 
 
 int main() {
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     try {
         std::freopen("input.txt", "r", stdin);
 
